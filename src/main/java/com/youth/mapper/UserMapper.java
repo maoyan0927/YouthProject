2c5861224955f074e9a61651f44c6261a38356a7
maoyan0927 <maoy0927@163.com>
2022-07-16 20:11:22 +0800

    change mapper.xml config

--- src/main/java/com/youth/mapper/UserMapper.java
+++ src/main/java/com/youth/mapper/UserMapper.java
@@ -3,7 +3,9 @@ package com.youth.mapper;
 
 import com.youth.Entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
