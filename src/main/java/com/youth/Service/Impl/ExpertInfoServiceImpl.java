2619d2dc5a21cea08c73cfefa83fce7777797678
张拓 <1102168414@qq.com>
2022-07-27 16:27:25 +0800

    专家实现的注释添加（@Service @Autowired）

--- src/main/java/com/youth/Service/Impl/ExpertInfoServiceImpl.java
+++ src/main/java/com/youth/Service/Impl/ExpertInfoServiceImpl.java
@@ -1,10 +1,17 @@
 package com.youth.Service.Impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.youth.Entity.ExpertInfo;
+import com.youth.Entity.User;
 import com.youth.Service.ExpertInfoService;
 import com.youth.mapper.ExpertInfoMapper;
+import com.youth.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class ExpertInfoServiceImpl implements ExpertInfoService {
+@Service
+public class ExpertInfoServiceImpl extends ServiceImpl<ExpertInfoMapper, ExpertInfo> implements ExpertInfoService {
+    @Autowired
     ExpertInfoMapper expertInfoMapper;
 
     public ExpertInfo getExpertInfoByPhone(String phone){
--- src/main/java/com/youth/mapper/ExpertInfoMapper.java
+++ src/main/java/com/youth/mapper/ExpertInfoMapper.java
@@ -3,7 +3,9 @@ package com.youth.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.youth.Entity.ExpertInfo;
 import com.youth.Entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ExpertInfoMapper extends BaseMapper<ExpertInfo> {
     ExpertInfo getExpertInfoByPhone (String phone);
 }
