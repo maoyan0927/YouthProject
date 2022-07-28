d87504c72de115b8381ba37509ff69030f1209bb
张拓 <1102168414@qq.com>
2022-07-28 10:30:30 +0800

    添加Iservice接口

--- src/main/java/com/youth/Service/ExpertInfoService.java
+++ src/main/java/com/youth/Service/ExpertInfoService.java
@@ -1,7 +1,8 @@
 package com.youth.Service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.youth.Entity.ExpertInfo;
 
-public interface ExpertInfoService {
+public interface ExpertInfoService extends IService<ExpertInfo> {
     ExpertInfo getExpertInfoByPhone(String phone);
 }
