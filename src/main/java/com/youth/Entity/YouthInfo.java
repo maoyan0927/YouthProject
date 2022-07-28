d01d2e942fb6dd716364a0c9b4169bfa0ad40760
张拓 <1102168414@qq.com>
2022-07-28 11:10:30 +0800

    青少年设置 主键自增

--- src/main/java/com/youth/Entity/YouthInfo.java
+++ src/main/java/com/youth/Entity/YouthInfo.java
@@ -1,9 +1,6 @@
 package com.youth.Entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -22,7 +19,7 @@ public class YouthInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @ApiModelProperty("孩童id")
-    @TableId(value = "youth_id")
+    @TableId(value = "youth_id", type = IdType.AUTO)
     private Integer youthId;
 
     @ApiModelProperty("孩童name")
