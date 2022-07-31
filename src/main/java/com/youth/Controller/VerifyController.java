0f366eada2591d8b83ef275f7e18fe7ef14918cb
maoyan0927 <maoy0927@163.com>
2022-07-31 22:06:34 +0800

    add expertInfo action

--- src/main/java/com/youth/Controller/VerifyController.java
+++ src/main/java/com/youth/Controller/VerifyController.java
@@ -3,6 +3,7 @@ package com.youth.Controller;
 
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -12,5 +13,9 @@ import org.springframework.web.bind.annotation.*;
 @Api(tags = "专家验证类")
 @CrossOrigin
 public class VerifyController {
+
+    @Autowired
+    private CalculateController calculateController;
+
 
 }
--- src/main/java/com/youth/Entity/TaskInfo.java
+++ src/main/java/com/youth/Entity/TaskInfo.java
@@ -1,4 +1,40 @@
 package com.youth.Entity;
 
-public class TaskInfo {
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@TableName("task_info")
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "任务属性")
+public class TaskInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty("任务id")
+    @TableId(value = "task_id",type = IdType.AUTO)
+    private Integer taskId;
+
+    @ApiModelProperty("识别Id")
+    private Integer recoId;
+
+    @ApiModelProperty("专家id")
+    private Integer expertId;
+
+    @ApiModelProperty("创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @ApiModelProperty("更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @ApiModelProperty("任务状态")
+    private Integer state;
 }
